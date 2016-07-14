6fad2952829cea04a8b72eeef2b85da046ce9e2a
Abhiram Abbineni <abhi@dimedev.com>
2016-07-14 15:50:00 -0500

    Initial Commit

---
+++ TestingGIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingGIT
+//
+//  Created by Abhiram Abbineni on 7/14/16.
+//  Copyright © 2016 abhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
